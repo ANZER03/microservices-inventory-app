8e7fc66528f5cd68fa9fca5300b1106c67802a53
Anouar Zerrik <an_zerrik@etu.enset-media.ac.ma>
2025-11-25 14:35:12 +0100

    Create the customer service

---
+++ customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,32 @@
+package ma.enset.customerservice;
+
+import ma.enset.customerservice.entities.Customer;
+import ma.enset.customerservice.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            new Customer();
+            customerRepository.save(Customer.builder()
+                    .email("an@gmail.com")
+                    .name("Anoir")
+                    .build());
+        };
+    }
+}
